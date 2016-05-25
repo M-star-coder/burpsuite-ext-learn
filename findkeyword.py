9d57106c08e5da38be3030a1a4b7e678f05e6b82
nohack <nohack@nohackdeMacBook-Air.local>
2016-05-25 17:05:18 +0800

    edit the py

--- findkeyword.py
+++ findkeyword.py
@@ -1,12 +1,13 @@
-keyword='1572608274'
-a=messageInfo.getResponse()
-newres=helpers.analyzeResponse(a)
-offset=newres.getBodyOffset()
-body=a[offset:]
-body_string=body.tostring()
-if (body_string.find(keyword)!=-1):
-	print "find keyword!!!!"
-	b=messageInfo.getRequest()
-	newreq=helpers.analyzeRequest(b)
-	requrl=newreq.getUrl()
-	print requrl
+if (messageIsRequest==0):
+	keyword=['1572608274','testkeyword']
+	a=messageInfo.getResponse()
+	newres=helpers.analyzeResponse(a)
+	offset=newres.getBodyOffset()
+	body=a[offset:]
+	body_string=body.tostring()
+	for onekey in keyword:
+		if (body_string.find(onekey)!=-1):
+			print "find keyword!!!!"+onekey
+			b=messageInfo.getRequest()
+			print b.tostring()
+			print "----------------------------------"*3
